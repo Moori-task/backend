64a1ce4c59febc37f85c84a6ad7d67b1e7ccc066
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-01-19 14:57:13 +0330

    feat(automatic_search): init reservation-based filtering

--- place/views/automatic_search.py
+++ place/views/automatic_search.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from rest_framework import serializers, viewsets
 
 from place.models.place import Place
@@ -5,7 +6,9 @@ from place.models.place import Place
 
 from django_filters import rest_framework as filters
 
-class PlaceFilter(filters.FilterSet):
+from place.models.reservation import ReserveSlot
+
+class BasicPlaceFilterSet(filters.FilterSet):
     class Meta:
         model = Place
         fields = {
@@ -13,6 +16,38 @@ class PlaceFilter(filters.FilterSet):
             'rate': ['gte'],
             'area_size': ['lte', 'gte']
         }
+
+class ReserveBasedPlaceFilterSet(filters.FilterSet):
+    def __init__(self, base_filter_set: "filters.FilterSet", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__base_filter_set = base_filter_set
+    
+    def filter_queryset(self, queryset):
+        queryset = self.__base_filter_set.filter_queryset(queryset)
+        queryset = super().filter_queryset(queryset)
+        if self.__queried(queryset):
+            queryset = self.__get_available_places(queryset)
+        return queryset
+    
+    def __get_available_places(self, queryset):
+        # assert queryset = Join Place, Reserve
+        reserved_places = queryset.filter(status=ReserveSlot.ReserveSlotStatus.reserved).distinct()
+        places = queryset.distinct()
+        available_places = places.exclude(code__in=reserved_places)
+        return available_places
+    
+    def __queried(self, queryset) -> bool:
+        # It should figure out whether a request is queried by reservation
+        # return queryset = Join Place, Reserve
+        # TODO improve
+        return True
+    
+    class Meta:
+        model = Place
+        fields = {
+            'reserved_slot__date': ['lte', 'gte']
+        }
+    
 
 class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
@@ -27,7 +62,7 @@ class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = PlaceSerializer
     permission_classes = []
     filter_backends = (filters.DjangoFilterBackend,)
-    filterset_class = PlaceFilter
+    filterset_class = BasicPlaceFilterSet
     
     def filter_queryset(self, queryset):
         return super().filter_queryset(queryset)[:5]
