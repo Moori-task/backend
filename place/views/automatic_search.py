d94657a9dc77f0760c3e9a3c8d1771b263251e64
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-01-19 18:18:37 +0330

    refactor(reserve-based-filter): make more use of superclasses

--- place/views/automatic_search.py
+++ place/views/automatic_search.py
@@ -3,34 +3,43 @@ from rest_framework import serializers, viewsets
 
 from place.models.place import Place
 
-
 from django_filters import rest_framework as filters
 from django_filters.conf import settings
+from django_filters.constants import EMPTY_VALUES
 from place.models.reservation import ReserveSlot
 
 
 class ReservationBasedFilter(filters.DateFromToRangeFilter):
-    def filter(self, qs, value):
-        qs = super().filter(qs, value)
-        if self.__queried(qs, value):
-            qs = self.__get_available_places(qs, value)
-        return qs
+    def filter(self, places, value):
+        places = super().filter(places, value)
+        if not self.filtered(places, value):
+            return places
+        return self.__get_available_places(places, value)
+    
+    def filtered(self, places, value):
+        return self.process_value(value) not in EMPTY_VALUES
 
     def __get_available_places(self, places, value):
-        # assert queryset = Join Place, Reserve
-        reserved_places = places.filter(
-            reserve_slots__status=ReserveSlot.ReserveSlotStatus.reserved,
-            reserve_slots__date__range=[value.start, value.stop],
-        )
-        available_places = places.exclude(code__in=reserved_places).distinct()
-        return available_places
-
-    def __queried(self, qs, value) -> bool:
-        # It should figure out whether a request is queried by reservation_date
-        # I checked out in the library and if there would be a querying,
-        # lookup_expr will be set to sth other than "DEFAULT" after finished function
-        # TODO improve
-        return self.lookup_expr != settings.DEFAULT_LOOKUP_EXPR
+        reserved_places = self.__get_reserved_places(places, value)
+        return places.exclude(code__in=reserved_places).distinct()
+    
+    def __get_reserved_places(self, places, value):
+        value = self.process_value(value)
+        lookup = "%s__%s" % (self.field_name, self.lookup_expr)
+        return places.filter(**{
+            lookup: value,
+            "reserve_slots__status":ReserveSlot.ReserveSlotStatus.reserved
+        })
+    
+    def process_value(self, value):
+        if value:
+            if value.start is not None and value.stop is not None:
+                return (value.start, value.stop)
+            elif value.start is not None:
+                return value.start
+            elif value.stop is not None:
+                return value.stop
+        return value
 
 
 class BasicPlaceFilterSet(filters.FilterSet):
